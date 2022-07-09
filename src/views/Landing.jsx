a578a1baa0de6ddef9d75c21a21780875306262b
pulgueta <retardix456@gmail.com>
2022-07-08 20:40:08 -0500

    Box

--- src/views/Landing.jsx
+++ src/views/Landing.jsx
@@ -1,4 +1,4 @@
-import { VStack, Heading, Text } from "@chakra-ui/react";
+import { VStack, Heading, Text, Button } from "@chakra-ui/react";
 
 export const Landing = () => {
   return (
@@ -10,6 +10,13 @@ export const Landing = () => {
       <Text color="white" py="24px" fontWeight="400" fontSize="2xl">
         Take this simple quiz and have fun testing how fast can you type!
       </Text>
+
+      <VStack bgColor="brand.800" p={16} rounded={16}>
+        <Text mb={8}>Test yourself clicking down here!</Text>
+        <Button bgColor="brand.700" color="white">
+          Test me!
+        </Button>
+      </VStack>
     </VStack>
   );
 };
